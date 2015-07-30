f2e51b666f60136a3fcdb593d72fe5e7e440c2e5
Chase McCarthy <chase@code0100fun.com>
2015-07-30 16:25:50 -0400

    Order casts by id

--- app/controllers/casts.js
+++ app/controllers/casts.js
@@ -1,6 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  casts: Ember.computed('model', function() {
+    return Ember.ArrayProxy.createWithMixins(Ember.SortableMixin, {
+      sortProperties: ['id'],
+      sortAscending: false,
+      content: this.get('model')
+    });
+  }),
   actions: {
     createCast(content, done) {
      const cast =  this.store.createRecord('cast', { content });
